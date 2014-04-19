515de8e64cb1bff20513541ec849bb300ce7fb21
luizpedone <luiz.pedone@gmail.com>
2014-04-19 18:59:47 -0300

    Minify assets.

---
+++ app/config/packages/ceesvanegmond/minify/config.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | App environments to not minify
+    |--------------------------------------------------------------------------
+    |
+    | These environments will not be minified and all individual files are
+    | returned
+    |
+    */
+
+    'ignore_environments' => array(
+	    // 'local',
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | CSS build path
+    |--------------------------------------------------------------------------
+    |
+    | Minify is an extention that can minify your css files into one build file.
+    | The css_builds_path property is the location where the builded files are
+    | stored. This is relative to your public path. Notice the trailing slash.
+    | Note that this directory must be writable.
+    |
+    */
+
+    'css_build_path' => '/css/builds/',
+
+    /*
+    |--------------------------------------------------------------------------
+    | JS build path
+    |--------------------------------------------------------------------------
+    |
+    | Minify is an extention that can minify your js files into one build file.
+    | The js_build_path property is the location where the builded files are
+    | stored. This is relative to your public path. Notice the trailing slash.
+    | Note that this directory must be writable.
+    |
+    */
+
+    'js_build_path' => '/js/builds/',
+
+);
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,13 +2,11 @@
 <html>
 <head>
     <title>Não Move - Exponha os problemas do seu transporte público</title>
-    <link rel="stylesheet" href="{{ URL::to('/') }}/css/bootstrap.min.css">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link href='http://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ URL::to('/') }}/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.min.css">
+    {{ Minify::stylesheet(array('/css/bootstrap.min.css', '/css/style.css')) }}
     <link rel="shortcut icon" href="{{ URL::to('/') }}/img/favicon.png" type="image/ico" />
-
 </head>
 <body>
     <header>
@@ -45,17 +43,14 @@
     <footer>
         &copy; 2014 Luiz Felipe Pedone
     </footer>
-    <script src="{{ URL::to('/') }}/js/jquery-1.11.0.min.js"></script>
-    <script src="{{ URL::to('/') }}/js/bootstrap.min.js"></script>
+    {{ Minify::javascript(array('/js/jquery-1.11.0.min.js', '/js/bootstrap.min.js')) }}
 	<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-29298388-6', 'luizpedone.com');
-  ga('send', 'pageview');
-
-</script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-29298388-6', 'luizpedone.com');
+      ga('send', 'pageview');
+    </script>
 </body>
 </html>
