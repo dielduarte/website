0977761a17f8685917e90e4c79fca00bf2c41301
luizpedone <luiz.pedone@gmail.com>
2014-07-01 22:29:53 -0300

    Correct config files

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('ae002107.dynamic.ppp.asahi-net.or.jp'),
+	'local' => ['ae002106.dynamic.ppp.asahi-net.or.jp'],
 
 ));
 
