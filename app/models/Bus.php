7ac4adb4c2d75b9eb86330776fbf26b2a37edfb2
luizpedone <luiz.pedone@gmail.com>
2014-07-01 22:31:10 -0300

    Fix home Bus list

--- app/models/Bus.php
+++ app/models/Bus.php
@@ -3,6 +3,25 @@
 class Bus extends \Eloquent {
 	protected $fillable = ['line', 'itinerary'];
 
+    /*
+     * Scopes
+     */
+    public function scopeActiveBusesList($query)
+    {
+        $query->remember(720, 'bus_lines')
+              ->active()
+              ->select(DB::raw('concat (line," (", itinerary, ")") as line_plus_itinerary, id'))
+              ->orderBy('line_plus_itinerary', 'ASC');
+    }
+
+    public function scopeActive($query)
+    {
+        $query->where('active', '=', true);
+    }
+
+    /*
+     * Relationships
+     */
     public function complaints()
     {
         return $this->hasMany('Complaint', 'bus_id');
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -55,10 +55,11 @@
                         Alguns campos não foram preenchidos corretamente. Eles estão marcados de vermelho.
                     </div>
                 @endif
+                {{ App::environment() }}
                 {{ Form::open(['url' => 'registrar-relato', 'data-parsley-validate']) }}
                     {{ Form::textField('name', 'Seu nome', Input::old('name')) }}
                     {{ Form::emailField('email', 'Seu e-mail (não será divulgado)', Input::old('email')) }}
-                    {{ Form::selectField('bus_id', 'Em qual linha foi o problema?', Bus::remember(720, 'bus_lines')->select(DB::raw('concat (line," (", itinerary, ")") as line_plus_itinerary, id'))->orderBy('line_plus_itinerary', 'ASC')->lists('line_plus_itinerary', 'id'), Input::old('line'), ['id' => 'combobox']) }}
+                    {{ Form::selectField('bus_id', 'Em qual linha foi o problema?', Bus::activeBusesList()->lists('line_plus_itinerary', 'id'), Input::old('line'), ['id' => 'combobox']) }}
                     {{ Form::selectField('reason_id', 'Qual é o motivo da reclamação?', Reason::remember(720)->orderBy('reason', 'ASC')->lists('reason', 'id'), Input::old('reason')) }}
                     {{ Form::textAreaField('story', 'Conte a sua história', Input::old('story')) }}
                     {{ Form::label('Código de Verificação') }}
